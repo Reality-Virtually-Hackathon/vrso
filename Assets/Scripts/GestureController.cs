4d9662b25539786220cbc16dc6dc7568137bfb52
Patrick Ryan <patrick@fancybeargames.com>
2017-10-07 13:30:14 -0400

    Working on hand gesture.

---
+++ Assets/Scripts/GestureController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureController : MonoBehaviour
+{
+    public enum Location
+    {
+        Up,
+        Down
+    }
+
+    private List<GameObject> m_upFingers = new List<GameObject>();
+    private List<GameObject> m_downFingers = new List<GameObject>();
+
+    public bool isOpen;
+    public bool opened;
+    public bool isClosed;
+    public bool closed;
+
+    public void AddFinger(Location a_location, GameObject a_finger)
+    {
+        switch (a_location)
+        {
+            case Location.Up:
+                if (!m_upFingers.Contains(a_finger))
+                {
+                    m_upFingers.Add(a_finger);
+                }
+                break;
+            case Location.Down:
+                if (!m_downFingers.Contains(a_finger))
+                {
+                    m_downFingers.Add(a_finger);
+                }
+                break;
+        }
+    }
+
+    public void RemoveFinger(Location a_location, GameObject a_finger)
+    {
+        switch (a_location)
+        {
+            case Location.Up:
+                if (m_upFingers.Contains(a_finger))
+                {
+                    m_upFingers.Remove(a_finger);
+                }
+                break;
+            case Location.Down:
+                if (m_downFingers.Contains(a_finger))
+                {
+                    m_downFingers.Remove(a_finger);
+                }
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if(opened)
+        {
+            opened = false;
+        }
+        if(closed)
+        {
+            closed = false;
+        }
+
+        int openCount = m_upFingers.Count;
+        int closedCount = m_downFingers.Count;
+
+        if(openCount >= 3)
+        {
+            isOpen = true;
+
+            if(isClosed)
+            {
+                opened = true;
+            }
+        }
+
+        if(closedCount >= 3)
+        {
+            isClosed = true;
+
+            if(isOpen)
+            {
+                closed = true;
+            }
+        }
+
+        if(openCount < 3)
+        {
+            isOpen = false;
+        }
+
+        if(closedCount < 3)
+        {
+            isClosed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GestureTriggers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureTriggers : MonoBehaviour
+{
+    private GestureController m_gestureController;
+    [SerializeField]
+    private GestureController.Location m_location;
+
+    private void Awake()
+    {
+        m_gestureController = GetComponentInParent<GestureController>();
+    }
+
+    private void OnTriggerEnter(Collider a_col)
+    {
+        m_gestureController.AddFinger(m_location, a_col.gameObject);
+    }
+
+    private void OnTriggerExit(Collider a_col)
+    {
+        m_gestureController.RemoveFinger(m_location, a_col.gameObject);
+    }
+}
